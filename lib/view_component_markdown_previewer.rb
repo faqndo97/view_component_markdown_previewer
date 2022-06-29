0998acb4948efb695f450fa873953dad4f206ad5
Facundo Espinosa <facundo.espinosa@valimail.com>
2022-06-29 19:56:39 -0300

    Initial commit

---
+++ lib/view_component_markdown_previewer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "view_component_markdown_previewer/version"
+
+module ViewComponentMarkdownPreviewer
+  class Error < StandardError; end
+  # Your code goes here...
+end
